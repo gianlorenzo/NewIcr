0df904346e8a9398c414112a0d9f15c0e1273457
gianlorenzo <gianlorenzo.didonato@gmail.com>
2018-10-25 18:35:48 +0200

    Delete import.sql

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
--- insert admin//admin
---insert into Administrator (id, password, role, username) VALUES ('1', 'admin', 'ROLE_ADMIN', 'admin');
--- insert student a//a
--- INSERT INTO student (dtype, id, name, password, role, school, school_group, section, surname, username, task_effettuati, tempo_effettuato) VALUES ('Student','1','a','$2a$10$3od8USTj1tmeaDBbYiRa4ubWhiH5.WeqBKvMDpgv7WMbge1HCuj2O','ROLE_USER','a','3','1','a','a',0,0);
-alter table result ADD CONSTRAINT task_id_unique UNIQUE (task_id);
-
